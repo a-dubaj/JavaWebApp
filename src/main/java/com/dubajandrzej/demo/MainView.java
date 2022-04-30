fda7e8fafee98a61f420cdffcf158ff91f785df9
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2022-04-30 16:14:57 +0200

    Added  initial component

--- src/main/java/com/dubajandrzej/demo/MainView.java
+++ src/main/java/com/dubajandrzej/demo/MainView.java
@@ -1,23 +1,29 @@
 package com.dubajandrzej.demo;
 
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.Paragraph;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.Route;
 
 @Route
 public class MainView extends VerticalLayout {
-    public MainView() {
-        var button = new Button("Click me");
-        var textField = new TextField();
+    private PersonRepository repository;
+    private TextField firstName = new TextField("First name: ");
+    private TextField lastName = new TextField("Last name: ");
+    private EmailField email = new EmailField("Email");
+    private Binder<Person> binder = new Binder<>(Person.class);
+    private Grid<Person> gird = new Grid<>(Person.class);
+    public MainView(PersonRepository repository) {
+        this.repository = repository;
 
-        add(new HorizontalLayout(textField, button));
+        add(getForm(), gird);
 
-        button.addClickListener(e -> {
-            add(new Paragraph("Hello, " + textField.getValue()));
-            textField.clear();
-        });
+    }
+
+    private Component getForm() {
+
     }
 }
--- src/main/java/com/dubajandrzej/demo/PersonRepository.java
+++ src/main/java/com/dubajandrzej/demo/PersonRepository.java
@@ -1,4 +1,7 @@
 package com.dubajandrzej.demo;
 
-public interface PersonRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
 }
