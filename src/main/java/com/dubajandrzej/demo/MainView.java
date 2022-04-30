20e8103936c672b8013001db1b1464fe71357f70
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2022-04-30 16:50:58 +0200

    Adding contacts to list

--- src/main/java/com/dubajandrzej/demo/MainView.java
+++ src/main/java/com/dubajandrzej/demo/MainView.java
@@ -9,13 +9,14 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
 import com.vaadin.flow.router.Route;
 
 @Route
 public class MainView extends VerticalLayout {
     private PersonRepository repository;
-    private TextField firstName = new TextField("First name: ");
-    private TextField lastName = new TextField("Last name: ");
+    private TextField firstName = new TextField("First name");
+    private TextField lastName = new TextField("Last name");
     private EmailField email = new EmailField("Email");
     private Binder<Person> binder = new Binder<>(Person.class);
     private Grid<Person> grid = new Grid<>(Person.class);
@@ -24,14 +25,30 @@ public class MainView extends VerticalLayout {
         this.repository = repository;
         grid.setColumns("firstName", "lastName", "email");
         add(getForm(), grid);
+        refreshGrid();
     }
 
     private Component getForm() {
         var layout = new HorizontalLayout();
         layout.setAlignItems(Alignment.BASELINE);
         var addButton = new Button("Add");
-        addButton.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+        addButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         layout.add(firstName, lastName, email, addButton);
+        binder.bindInstanceFields(this);
+        addButton.addClickListener(click -> {
+            try {
+                var person = new Person();
+                binder.writeBean(person);
+                repository.save(person);
+                refreshGrid();
+            } catch (ValidationException e) {
+                //
+            }
+        });
         return layout;
+    }
+
+    private void refreshGrid() {
+        grid.setItems(repository.findAll());
     }
 }
